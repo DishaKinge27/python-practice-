e63713db5bfaaf9864ce5479eb991bc30ebf2641
DishaKinge27 <dishakinge27@gmail.com>
2026-02-27 19:26:50 +0530

    Added digit extraction program

---
+++ fundamentals/fundamentals/digit_extraction.py
@@ -0,0 +1,8 @@
+# Digit Extraction Example 
+num=746
+print("The number is: ",num)
+string=str(num)
+print("First digit: ",string[0])
+print("Second digit: ",string[1])
+print("Third digit: ",string[2])
+#here we first changed type of num to string so that we can apply the string properties 
