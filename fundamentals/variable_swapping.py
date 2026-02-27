0b47869ac71f43fb73d5811584a691cfc137800b
DishaKinge27 <dishakinge27@gmail.com>
2026-02-27 19:33:01 +0530

    Added variable swapping example

---
+++ fundamentals/variable_swapping.py
@@ -0,0 +1,9 @@
+#variable swapping example 
+var1=27
+var2=45
+print("var1: ",var1)
+print("var2: ",var2)
+print("After Swapping:")
+var1,var2=var2,var1 #here we swap the values of variables without using third variable 
+print("var1: ",var1)
+print("var2: ",var2)
