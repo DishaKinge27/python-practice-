08b3dab2b9a272c3b983e324e3428f4f11988068
DishaKinge27 <dishakinge27@gmail.com>
2026-02-27 19:17:04 +0530

    type_casting.py

--- fundamentals/type_casting.py
+++ fundamentals/type_casting.py
@@ -1,1 +1,10 @@
+#type casting example
+marks_int=87 
+print("marks in integer: ",marks_int)
+print(type(marks_int)) 
+
+marks_float= float(marks_int) #here we changed integer to float, this is called as typecasting 
+
+print("marks in float: ",marks_float)
+print(type(marks_float))
 
