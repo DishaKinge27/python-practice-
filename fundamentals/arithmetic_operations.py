ef127d6800b3ca6b3c2884f7ed980f301f01630d
DishaKinge27 <dishakinge27@gmail.com>
2026-02-27 19:19:26 +0530

    arithmetic_operations.py

---
+++ fundamentals/arithmetic_operations.py
@@ -0,0 +1,11 @@
+# Arithmetic Operations Example
+a=728
+b=893
+print(a+b)   #addition
+print(a-b)   #subtraction
+print(a*b)   #multiplication
+print(a/b)   #division_gives result in float
+print(a//b)  #division_gives result in int
+print(a%b)   #modulus_gives remainder 
+c= a*283/3-b+27#operator precedence 
+print(c) 
